1619810f17f04c63618b9426db00c1b99974d75d
MateusNazareth <mateusalves85@yahoo.com.br>
2023-11-11 01:14:49 -0400

    return to the original dinamic mode

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,8 +5,11 @@ app_name = 'portfolio_app_name'
 
 urlpatterns = [
 
-    path('<int:proj_id>/', views.detail2, name="detail2"),
-    path('projeto1', views.projeto1, name='projeto1'),
-    path('projeto2', views.projeto2, name='projeto2'),
-    path('projeto3', views.projeto3, name='projeto3'),
+    path('<int:proj_id>/', views.detail2, name='detail2'),
+
 ]
+
+    # path('projeto1', views.projeto1, name='projeto1'),
+    # path('projeto2', views.projeto2, name='projeto2'),
+    # path('projeto3', views.projeto3, name='projeto3'),
+    # path('projeto4', views.projeto4, name='projeto4'),
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render, get_object_or_404
 
 def home(request):
     projects = Project.objects.all()     # pega todos os objetos do banco de dados!
-    return render(request, 'portfolio/home.html', {'projects': projects})
+    return render(request, 'portfolio/home_backup.html', {'projects': projects})
+
 
 
 def detail2(request, proj_id):
@@ -14,13 +15,16 @@ def detail2(request, proj_id):
     return render(request, 'portfolio/detail2.html', {'project': project})
 
 # Projects
-def projeto1(request):
-    return render(request, 'portfolio/projeto1.html')
+# def projeto1(request):
+#     return render(request, 'portfolio/projeto1.html')
 
-def projeto2(request):
-    return render(request, 'portfolio/projeto2.html')
+# def projeto2(request):
+#     return render(request, 'portfolio/projeto2.html')
 
-def projeto3(request):
-    return render(request, 'portfolio/projeto3.html')
+# def projeto3(request):
+#     return render(request, 'portfolio/projeto3.html')
+
+# def projeto4(request):
+#     return render(request, 'portfolio/projeto4.html')
 
 
