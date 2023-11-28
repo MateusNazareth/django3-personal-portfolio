903af88697560a6d9a69a6fefbb294c2c9d15425
MateusNazareth <mateusalves85@yahoo.com.br>
2023-11-28 01:33:23 -0400

    debug set to True

--- personal_portifolio/settings.py
+++ personal_portifolio/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e)!9ni$v$dg1yn8(^o&pfp3#=_i=zmk8$7+=n@9v--ew=n6fig'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ["mateusalves85.pythonanywhere.com"]
-# ALLOWED_HOSTS = ['.vercel.app']
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
+
 
 
 
